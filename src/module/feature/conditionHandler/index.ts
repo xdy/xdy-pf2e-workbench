cbc56a4ef4827cdbf51421392f369b296b874726
Jonas Karlsson <xdy@users.noreply.github.com>
2022-04-08 19:09:12 +0200

    feat: If the setting to automatically reduce Frightened condition at the end of turn is enabled the value of Frightened won't be reduced below the value in the flag "xdy-pf2e-workbench.condition.frightened.min". A sample Effect with a Rules Element that does this has been included in the compendium "xdy-pf2e-workbench-items", it's titled "Effect: Dirge of Doom". It contains this Rules Element: ``` {"key":"ActiveEffectLike","mode":"add","path":"flags.xdy-pf2e-workbench.condition.frightened.min","value":1} ```
    Fixes #203

--- src/module/feature/conditionHandler/index.ts
+++ src/module/feature/conditionHandler/index.ts
@@ -1,10 +1,15 @@
 import { CombatantPF2e } from "@module/encounter";
 import { shouldIHandleThis } from "../../utils";
+import { MODULENAME } from "../../xdy-pf2e-workbench";
 
 export async function reduceFrightened(combatant: CombatantPF2e) {
     if (combatant && combatant.actor && shouldIHandleThis(combatant.isOwner ? game.user?.id : null)) {
         if (combatant.actor.hasCondition("frightened")) {
-            await combatant.actor.decreaseCondition("frightened");
+            const minimumFrightened = <number>combatant.actor?.getFlag(MODULENAME, "condition.frightened.min") ?? 0;
+            const currentFrightened = combatant.actor?.getCondition("frightened")?.value ?? 0;
+            if (currentFrightened - 1 >= minimumFrightened) {
+                await combatant.actor.decreaseCondition("frightened");
+            }
         }
     }
 }
