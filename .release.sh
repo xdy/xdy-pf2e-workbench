7a23bec529723be9a43972d6c949286b310c33be
Jonas Karlsson <xdy@users.noreply.github.com>
2022-08-10 10:42:37 +0200

    fix: Trying to fix download link.

--- .release.sh
+++ .release.sh
@@ -1,6 +1,6 @@
 SEARCH_PATTERN='(\s\"(manifest|download)\"\: \"https:\/\/github.com\/xdy\/xdy-pf2e-workbench\/releases\/).*(\/(module.json|xdy-pf2e-workbench.zip)\",)'
 BETA_REPLACE="\1download/v$1\3"
-MAIN_REPLACE="\1latest/download\3"
+#MAIN_REPLACE="\1latest/download\3"
 
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" module.json &&
     sed -i -r s"~$SEARCH_PATTERN~$BETA_REPLACE~" module.json &&
