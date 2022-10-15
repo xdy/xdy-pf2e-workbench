80d3372a60b8f91628680e802a81e5d730933a29
Jonas Karlsson <xdy@users.noreply.github.com>
2022-10-15 15:55:22 +0200

    fix: Switch to always use latest version in the manifest.
    I.e. if you want to roll back to a previous version, figure it out yourself. :)
    
    Fixes #451

--- .release.sh
+++ .release.sh
@@ -1,11 +1,13 @@
 SEARCH_PATTERN='(\s\"(manifest|download)\"\: \"https:\/\/github.com\/xdy\/xdy-pf2e-workbench\/releases\/).*(\/(module.json|xdy-pf2e-workbench.zip)\",)'
 #For version specific download
-MAIN_REPLACE="\1download/v$1\3"
+VERSION_MAIN_REPLACE="\1download/v$1\3"
 #For latest download
-#MAIN_REPLACE="\1latest/download\3"
+LATEST_MAIN_REPLACE="\1latest/download\3"
+
+# Try to remember to switch to VERSION_MAIN_REPLACE just before major changes for those who remain on old foundry versions (i.e. when latest won't work)
 
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" module.json &&
-    sed -i -r s"~$SEARCH_PATTERN~$MAIN_REPLACE~" module.json &&
+    sed -i -r s"~$SEARCH_PATTERN~$LATEST_MAIN_REPLACE~" module.json &&
     cp module.json dist &&
     sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
     npm install &&
