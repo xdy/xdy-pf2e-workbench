781d705091d5f8e9fd625f68c16e3a7e3fef2408
Jonas Karlsson <xdy@users.noreply.github.com>
2023-05-31 19:07:36 +0200

    chore: Fix sed in release script so it also updates version number in package.json

--- .release.sh
+++ .release.sh
@@ -5,11 +5,11 @@ VERSION_MAIN_REPLACE="\1download/v$1\3"
 #For latest download
 LATEST_MAIN_REPLACE="\1latest/download\3"
 
-sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/module.json &&
+sed -i 's/"version": "[^"]*"/"version": "$1"/' static/module.json &&
 sed -i -r s"~${MANIFEST_SEARCH_PATTERN}~${LATEST_MAIN_REPLACE}~" static/module.json &&
 sed -i -r s"~${DOWNLOAD_SEARCH_PATTERN}~${VERSION_MAIN_REPLACE}~" static/module.json &&
 cp static/module.json dist &&
-sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
+sed -i 's/"version": "[^"]*"/"version": "$1"/' package.json &&
 npm install &&
 cd dist || exit &&
 zip -r xdy-pf2e-workbench.zip ./* &&
