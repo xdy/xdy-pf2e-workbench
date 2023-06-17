9a12a01465303e1bc6375c835e5d05136ba043aa
Jonas Karlsson <xdy@users.noreply.github.com>
2023-06-17 14:45:23 +0200

    fix: The option to sort actions works again, and now sorts exploration and downtime actions as well.

--- src/module/xdy-pf2e-workbench.ts
+++ src/module/xdy-pf2e-workbench.ts
@@ -399,10 +399,12 @@ Hooks.once("ready", () => {
         const wrapped = proto.getData;
         proto.getData = async function (options) {
             const data = await wrapped.call(this, options);
-            const actions = data.actor.actions;
-            actions.action.actions.sort((a, b) => a.name.localeCompare(b.name));
-            actions.reaction.actions.sort((a, b) => a.name.localeCompare(b.name));
-            actions.free.actions.sort((a, b) => a.name.localeCompare(b.name));
+            const actions = data["actions"];
+            actions.combat.action.actions.sort((a, b) => a.name.localeCompare(b.name));
+            actions.combat.reaction.actions.sort((a, b) => a.name.localeCompare(b.name));
+            actions.combat.free.actions.sort((a, b) => a.name.localeCompare(b.name));
+            actions.exploration.sort((a, b) => a.name.localeCompare(b.name));
+            actions.downtime.sort((a, b) => a.name.localeCompare(b.name));
             return data;
         };
 
