555e0edfeabde523a0fe4b2f64651b4be35670ee
Jonas Karlsson <xdy@users.noreply.github.com>
2023-07-20 16:35:13 +0200

    chore: Simplify shouldIHandleThis a bit.

--- src/module/utils.ts
+++ src/module/utils.ts
@@ -124,14 +124,13 @@ export function debounce(callback, wait) {
 
 export function shouldIHandleThis(actor) {
     if (!actor) return null;
-    const currentUser = game.users.get(game.user.id, { strict: true });
-    const activeUsers = game.users.filter((u) => u.active);
-    const assignedUser = activeUsers.find((u) => u.character === actor);
-    const firstGM = activeUsers.find((u) => u.isGM);
-    const anyoneWithPermission = activeUsers.find((u) => actor.canUserModify(u, "update"));
+    const currentUser = game.users.current;
+    const activePlayers = game.users.players.filter((u) => u.active);
+    const assignedUser = activePlayers.find((u) => u.character === actor);
+    const anyoneWithPermission = activePlayers.find((u) => actor.canUserModify(u, "update"));
     const updater =
-        currentUser.active && actor.canUserModify(currentUser, "update")
+        currentUser?.active && actor.canUserModify(currentUser, "update")
             ? currentUser
-            : assignedUser ?? firstGM ?? anyoneWithPermission ?? null;
+            : assignedUser ?? game.users.activeGM ?? anyoneWithPermission ?? null;
     return game.user.id === updater?.id;
 }
