da96350126056b95b513385f22250e4dce66a735
Jonas Karlsson <xdy@users.noreply.github.com>
2023-07-22 23:59:38 +0200

    feat: Adds the original macro documentation (if any) to the top of the macros in the 'Symon-provided macros' compendium. You do need to reimport/readd to macro bar to get this.

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -28,9 +28,9 @@ function copyFolder(source: string, target: string) {
         fs.appendFileSync(
             targetPath,
             `\n/* # source "https://gitlab.com/symonsch/my-foundryvtt-macros/-/tree/main/${path.basename(
-                path.dirname(sourcePath)
+                path.dirname(sourcePath),
             )}/${file}" - Fetched on ${new Date().toISOString()} */
-                `
+                `,
         );
     }
 }
@@ -52,11 +52,11 @@ function buildAsymonousPack() {
 
     copyFolder(
         path.resolve(".", asymonousSource[0]),
-        path.resolve(path.resolve(outDir, "packs/generated/asymonous-benefactor-macros"))
+        path.resolve(path.resolve(outDir, "packs/generated/asymonous-benefactor-macros")),
     );
     copyFolder(
         path.resolve(".", asymonousSource[1]),
-        path.resolve(path.resolve(outDir, "packs/generated/asymonous-benefactor-macros"))
+        path.resolve(path.resolve(outDir, "packs/generated/asymonous-benefactor-macros")),
     );
 
     const folders = getFolders(path.resolve(outDir, "packs/generated"))
@@ -75,9 +75,16 @@ function buildAsymonousPack() {
                 continue;
             }
 
+            const contents = fs.readFileSync(filePath, { encoding: "utf8" });
+            const documentation = contents.match(/\/\*[\s\S]*?\*\//);
+
             try {
                 const macroName = path.parse(file).name;
                 const importMacro = `/** This compendium link macro will always call the most recent version from the compendium included with this module meaning you do not need to reimport newer versions. The source of the macros that get called is https://gitlab.com/symonsch/my-foundryvtt-macros/-/tree/main/PF2e */
+/* Start of documentation from the original macro: */
+${documentation ? documentation[0] : "/* There is no documentation in the macro. */"}
+/* End of original macro documentation. */
+
     async function _executeMacroByName(
         macroName,
         compendiumName = "xdy-pf2e-workbench.asymonous-benefactor-macros-internal"
@@ -100,7 +107,6 @@ function buildAsymonousPack() {
 
     /* This compendium link macro is based on one originally posted by DrentalBot: https://discord.com/channels/880968862240239708/880975811279204402/910490804554973274; and modified by Mark Pearce https://discord.com/channels/880968862240239708/880969174661353484/972962446098702376 */
     `;
-                const contents = fs.readFileSync(filePath, { encoding: "utf8" });
                 const map = new Map<string, string>();
                 map.set("2-Action Harm", "systems/pf2e/icons/spells/harm.webp");
                 map.set("2-Action Heal v2", "systems/pf2e/icons/spells/heal.webp");
@@ -117,13 +123,13 @@ function buildAsymonousPack() {
                 map.set("Countdown-Cooldown", "icons/svg/dice-target.svg");
                 map.set(
                     "Custom Saves and Skill Checks",
-                    "systems/pf2e/icons/equipment/held-items/abadars-flawless-scale.webp"
+                    "systems/pf2e/icons/equipment/held-items/abadars-flawless-scale.webp",
                 );
                 map.set("Double Slice", "systems/pf2e/icons/spells/echoing-weapon.webp");
                 map.set("Dual Class", "systems/pf2e/icons/spells/guidance.webp");
                 map.set(
                     "Eldritch Shot",
-                    "systems/pf2e/icons/equipment/consumables/ammunition/spellstrike-ammunition.webp"
+                    "systems/pf2e/icons/equipment/consumables/ammunition/spellstrike-ammunition.webp",
                 );
                 map.set("Flurry of Blows", "systems/pf2e/icons/features/classes/flurry-of-blows.webp");
                 map.set("Level Based DCs", "systems/pf2e/icons/equipment/held-items/radiant-spark.webp");
@@ -145,7 +151,7 @@ function buildAsymonousPack() {
                 map.set("Spell DCs by Level", "systems/pf2e/icons/features/classes/conflux-spells.webp");
                 map.set(
                     "Spellsling",
-                    "systems/pf2e/icons/equipment/consumables/ammunition/energized-cartridge-electricity.webp"
+                    "systems/pf2e/icons/equipment/consumables/ammunition/energized-cartridge-electricity.webp",
                 );
                 map.set("Spellstrike", "systems/pf2e/icons/features/classes/spellstrike.webp");
                 map.set("Treat Wounds and Battle Medicine", "systems/pf2e/icons/conditions/wounded.webp");
@@ -156,12 +162,12 @@ function buildAsymonousPack() {
 
                 // eslint-disable-next-line
                 let jsonInternal = `{"_id": "${myRandomId()}", "actorIds": [], "author": "${myRandomId()}", "command": ${JSON.stringify(
-                    contents
+                    contents,
                 )},"flags": {},"img":"icons/svg/trap.svg","name": "XDY DO_NOT_IMPORT ${macroName}","permission": {"default": 1},"scope": "global","type": "script"}`;
                 linesInternal.push(jsonInternal);
                 // eslint-disable-next-line
                 let json = `{"_id": "${myRandomId()}", "actorIds": [], "author": "${myRandomId()}", "command": ${JSON.stringify(
-                    importMacro
+                    importMacro,
                 )},"flags": {},"img":"${img}","name": "${macroName}","permission": {"default": 1},"scope": "global","type": "script"}`;
                 lines.push(json);
             } catch (err) {
@@ -195,7 +201,7 @@ function buildCustomizableMacrosPack() {
 
             // eslint-disable-next-line
             let json = `{"_id": "${myRandomId()}", "actorIds": [], "author": "${myRandomId()}", "command": ${JSON.stringify(
-                contents
+                contents,
             )},"flags": {},"img":"${img}","name": "${macroName}","permission": {"default": 1},"scope": "global","type": "script"}`;
             lines.push(json);
         } catch (err) {
@@ -228,7 +234,7 @@ function buildInternalUtilityMacrosPack() {
 
             // eslint-disable-next-line
             let json = `{"_id": "${myRandomId()}", "actorIds": [], "author": "${myRandomId()}", "command": ${JSON.stringify(
-                contents
+                contents,
             )},"flags": {},"img":"${img}","name": "${macroName}","permission": {"default": 1},"scope": "global","type": "script"}`;
             lines.push(json);
         } catch (err) {
