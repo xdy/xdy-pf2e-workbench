7912cad4c65228f6fce3f968916a001409886610
Trent Piepho <tpiepho@gmail.com>
2024-04-16 01:04:20 -0700

    Filter the FollowTheExpert choices automatically from the target
    If a target is selected when using FTE, filter the allowed skill choices
    for the ChoiceSet to just those the target (i.e., the titular "expert")
    has at expert or better proficiency.
    
    Then add predicates to all the proficiency rank choices that check if
    the selected skill is one of the skils the target had at that
    proficiency rank.  This means only one of the choices should have a
    predicate that passes, and with just one choice the dialog will be
    skipped and the rank is effectively selected automatically.
    
    Example choice would look like:
    {
      "label": "PF2E.ProficiencyLevel2",
      "sort": 1,
      "value": 2,
      "predicate": {
        "or": [
          {
    	"eq": [
    	  "{item|flags.pf2e.rulesSelections.followTheExpertSkill}",
    	  "intimidation"
    	]
          }
        ]
      }
    }
    
    In this case, the target has just one still at Expert level,
    intimidation.  If that was chosen as the skill, then the predicate
    passes and the choice is allowed.  If another skill was chosen, then
    expert is not allowed.
    
    If there is no target, then it just adds the effect the same as it did
    before.

--- src/module/feature/macros/follow-the-expert.ts
+++ src/module/feature/macros/follow-the-expert.ts
@@ -1,8 +1,25 @@
 import type { Action, ActionVariant, ActionUseOptions, ActionMessageOptions } from "@actor/actions/types.d.ts";
+import type { ActorPF2e } from "@actor";
 import type { ChatMessagePF2e } from "@module/chat-message/document.d.ts";
 import type { ActionTrait } from "@item/ability/index.d.ts";
 import type { EffectPF2e } from "@item";
+import type { EffectSource } from "@item/effect/data.d.ts";
+import type { ChoiceSetSource } from "@module/rules/rule-element/choice-set/data.d.ts";
+import type { PickableThing } from "@module/apps/pick-a-thing-prompt.d.ts";
+import type { RawPredicate } from "@system/predication.d.ts";
 import { MODULENAME } from "../../xdy-pf2e-workbench.js";
+
+// IDK why choices isn't in ChoiceSetSource
+interface ChoiceSetSourceWithChoices extends ChoiceSetSource {
+    // Maybe this should have been UninflatedChoiceSet as the type, but that has issues,
+    // see below.
+    choices: PickableThingSource[];
+}
+
+// UninflatedChoiceSet can be PickableThing[], but PickableThing contains a Predicate.
+// We don't have a Predciate in the rule source, we have a RawPredicate.  There doesn't
+// seem to be an existing type for the source of a choice.
+type PickableThingSource = Omit<PickableThing, "predicate"> & { predicate?: RawPredicate; sort?: number };
 
 class FollowTheExpertAction implements Action {
     readonly img: string;
@@ -25,6 +42,48 @@ class FollowTheExpertAction implements Action {
         this.effect = "Compendium.pf2e.other-effects.VCSpuc3Tf3XWMkd3" as ItemUUID; // Effect: Follow The Expert
     }
 
+    private fixupRules(effect: EffectSource, expert: ActorPF2e) {
+        const expertSkills = expert.skills;
+        // Skilless expert? Don't follow them! Typescript complains.
+        if (!expertSkills) return;
+        // List of Lores that meet FTE minimum proficiency
+        const minLevel = 2;
+        const lores = Object.values(expertSkills)
+            .filter((s) => s.lore && (s.rank ?? 0) >= minLevel)
+            .map((s) => ({ label: s.label, value: s.slug }));
+
+        const skillRule = effect.system.rules.find(
+            (e: ChoiceSetSource) => e.key === "ChoiceSet" && e.flag === "followTheExpertSkill",
+        ) as ChoiceSetSourceWithChoices | null;
+        const profRule = effect.system.rules.find(
+            (e: ChoiceSetSource) => e.key === "ChoiceSet" && e.flag === "followTheExpertProficiency",
+        ) as ChoiceSetSourceWithChoices | null;
+        if (!skillRule || !profRule) throw new Error("Effect: FTE did not have expected rules");
+
+        // Filter skill choices that don't meet FTE minimum proficiency, default 2 (expert), and add lores
+        skillRule.choices = skillRule.choices
+            .filter((c) => (expertSkills[c.value as string]?.rank ?? 0) >= minLevel)
+            .concat(lores);
+
+        // Make proficiency choices predicate on having chosen a skill with that proficiency.
+        // This should result in only one choice being allowed, thus skipping the dialog.
+        const skills = skillRule.choices.map((c) => c.value) as string[];
+        for (const choice of profRule.choices) {
+            const skillsAtLevel = skills.filter((s) => expertSkills[s].rank === choice.value);
+            if (skillsAtLevel.length > 0) {
+                choice.predicate = [
+                    {
+                        or: skillsAtLevel.map((s) => ({
+                            eq: ["{item|flags.pf2e.rulesSelections.followTheExpertSkill}", s],
+                        })),
+                    },
+                ];
+            } else {
+                choice.predicate = ["disabled"];
+            }
+        }
+    }
+
     async use(_options?: Partial<ActionUseOptions>): Promise<unknown> {
         const source = (await fromUuid(this.effect)) as EffectPF2e | null;
         if (!source) throw new Error(`Effect ${this.effect} not found!`);
@@ -35,6 +94,11 @@ class FollowTheExpertAction implements Action {
         if (actors.length === 0 && game.user.character) actors.push(game.user.character);
         if (actors.length === 0) {
             return ui.notifications.error("PF2E.ErrorMessage.NoTokenSelected", { localize: true });
+        }
+
+        const target = game.user.targets.first();
+        if (target?.actor) {
+            this.fixupRules(effect, target.actor);
         }
 
         for (const actor of actors) {
