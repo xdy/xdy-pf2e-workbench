67799b1aa7ffd9cc04421966fb506c30e0fb200d
Jonas Karlsson <xdy@users.noreply.github.com>
2024-06-30 12:10:14 +0200

    fix: Breath Weapon reminder works again.
    Fixes #1347

--- src/module/feature/reminders/reminderBreathWeapon.ts
+++ src/module/feature/reminders/reminderBreathWeapon.ts
@@ -1,97 +1,76 @@
-// import { MODULENAME } from "../../xdy-pf2e-workbench.js";
 import { ChatMessagePF2e } from "@module/chat-message/index.js";
-// import { isFirstGM, myRandomId } from "../../utils.js";
-// import { TokenDocumentPF2e } from "@scene/index.js";
+import { isFirstGM, myRandomId } from "../../utils.js";
+import { TokenDocumentPF2e } from "@scene/index.js";
+import { MODULENAME } from "../../xdy-pf2e-workbench.js";
 
-export async function reminderBreathWeapon(_message: ChatMessagePF2e) {
-    // TODO Fix
-    // const messageContent = message.content;
-    // const activeCombatAvailable = isFirstGM() && messageContent && game.combats && game.combats.active;
-    //
-    // if (activeCombatAvailable) {
-    //     const token: TokenDocumentPF2e = getSpeakerToken(message);
-    //     const activeActor = token?.actor;
-    //
-    //     if (activeActor) {
-    //         const diceFormulaMatch = retrieveDiceFormulaMatch(messageContent);
-    //         const diceValue = diceFormulaMatch ? `1d${diceFormulaMatch[1]}` : "";
-    //
-    //         if (diceValue) {
-    //             const effectDetails = getEffectDetails(activeActor, messageContent, diceValue);
-    //             await activeActor?.createEmbeddedDocuments("Item", [effectDetails]);
-    //         }
-    //     }
-    // }
+export async function reminderBreathWeapon(message: ChatMessagePF2e) {
+    const messageContent = message.content;
+    const activeCombatAvailable = isFirstGM() && messageContent && game.combats && game.combats.active;
+
+    if (activeCombatAvailable) {
+        const token: TokenDocumentPF2e = <TokenDocumentPF2e>canvas?.scene?.tokens.get(<string>message.speaker.token);
+        if (token?.actor) {
+            let rounds;
+            if (messageContent.includes("data-tooltip") && messageContent.includes("data-roll")) {
+                const regex = /<i class="fas fa-dice-d20"><\/i>(\d).* rounds/;
+                const matches = messageContent.match(regex);
+                if (matches && matches.length > 1) {
+                    rounds = Number(matches[1]);
+                }
+            } else {
+                let diceValue: any;
+                const diceFormulaMatch = messageContent.match(/1d([46])( rounds| recharge|<\/a> rounds)/i);
+                if (diceFormulaMatch && diceFormulaMatch[1]) {
+                    diceValue = diceFormulaMatch[1];
+                }
+                const roll = await new Roll(`1d${diceValue}`).roll();
+                rounds = roll.total;
+            }
+            if (rounds) {
+                const data: any = await getEffectDetails(token.actor, messageContent, rounds);
+                await token.actor?.createEmbeddedDocuments("Item", [data]);
+            }
+        }
+    }
 }
 
-// function getSpeakerToken(message: ChatMessagePF2e): TokenDocumentPF2e {
-//     return <TokenDocumentPF2e>canvas?.scene?.tokens.get(<string>message.speaker.token);
-// }
-//
-// function retrieveDiceFormulaMatch(messageContent: string): RegExpMatchArray | null {
-//     // The last one is to support the current format in some third party products.
-//     return messageContent.match(/1d([46])( rounds| recharge|<\/a> rounds)/i);
-// }
-//
-// function getEffectDetails(activeActor, messageContent: string, diceValue: string) {
-//     const titleRetrieved = messageContent.match(/>(.*?) <span class="action-glyph"/);
-//
-//     return {
-//         type: "effect",
-//         name: getEffectName(titleRetrieved),
-//         img: "systems/pf2e/icons/spells/dragon-breath.webp",
-//         system: getSystemProperties(activeActor, diceValue),
-//     };
-// }
-//
-// function getEffectName(titleRetrieved: RegExpMatchArray | null): string {
-//     if (titleRetrieved) {
-//         return game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.used`) + titleRetrieved[1];
-//     } else {
-//         return (
-//             game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.used`) +
-//             game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.defaultName`)
-//         );
-//     }
-// }
-//
-// function getSystemProperties(activeActor, diceValue: string) {
-//     return {
-//         tokenIcon: { show: true },
-//         duration: getDurationProperties(diceValue),
-//         description: {
-//             value: `<h2>${game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.name`)}</h2>`,
-//         },
-//         source: {
-//             value: game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.defaultName`),
-//         },
-//         rules: [],
-//         slug: `xdy-breath-weapon-reminder-${myRandomId()}`,
-//         unidentified: getUnidentifiedProperty(activeActor),
-//         traits: {
-//             value: [],
-//         },
-//         level: {
-//             value: activeActor.level ?? 0,
-//         },
-//         badge: null,
-//         context: null,
-//     };
-// }
-//
-// async function getDurationProperties(diceValue: string) {
-//     const roll = await new Roll(diceValue).roll();
-//     return {
-//         value: roll.total + 1,
-//         unit: "rounds",
-//         sustained: false,
-//         expiry: "turn-start",
-//     };
-// }
-//
-// function getUnidentifiedProperty(activeActor) {
-//     return (
-//         game.settings.get(MODULENAME, "reminderBreathWeaponHidden") &&
-//         !game.actors?.party?.members.map((m) => m.id).includes(activeActor.id)
-//     );
-// }
+async function getEffectDetails(activeActor, messageContent: string, diceValue) {
+    const titleRetrieved = messageContent.match(/>(.*?) <span class="action-glyph"/);
+
+    return {
+        type: "effect",
+        name: titleRetrieved
+            ? game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.used`) + titleRetrieved[1]
+            : game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.used`) +
+              game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.defaultName`),
+        img: "systems/pf2e/icons/spells/dragon-breath.webp",
+        system: {
+            tokenIcon: { show: true },
+            duration: {
+                value: diceValue,
+                unit: "rounds",
+                sustained: false,
+                expiry: "turn-start",
+            },
+            description: {
+                value: `<h2>${game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.name`)}</h2>`,
+            },
+            source: {
+                value: game.i18n.localize(`${MODULENAME}.SETTINGS.reminderBreathWeapon.defaultName`),
+            },
+            rules: [],
+            slug: `xdy-breath-weapon-reminder-${myRandomId()}`,
+            unidentified:
+                game.settings.get(MODULENAME, "reminderBreathWeaponHidden") &&
+                !game.actors?.party?.members.map((m) => m.id).includes(activeActor.id),
+            traits: {
+                value: [],
+            },
+            level: {
+                value: activeActor.level ?? 0,
+            },
+            badge: null,
+            context: null,
+        },
+    };
+}
