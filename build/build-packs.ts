cc6d4eb7cafaf7c167d86433947df1f48fdd4a58
Jonas Karlsson <xdy@users.noreply.github.com>
2024-07-15 08:15:59 +0200

    chore: Add icon for new macro, Let Fate Decide.

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -141,6 +141,7 @@ ${documentation ? documentation[0] : "/* There is no documentation in the macro.
                 map.set("Marshal Stances", "systems/pf2e/icons/features/feats/dread-marshal-stance.webp");
                 map.set("Modded Countdown Cooldown", "systems/pf2e/icons/spells/time-beacon.webp");
                 map.set("Ooze Split", "systems/pf2e/icons/spells/blackfingers-blades.webp");
+                map.set("Let Fate Decide", "icons/magic/control/energy-stream-link-white.webp");
                 map.set("Random Encounter Builder", "systems/pf2e/icons/equipment/held-items/games.webp");
                 map.set("Ration Consumer", "systems/pf2e/icons/equipment/adventuring-gear/rations.webp");
                 map.set("Recall Knowledge", "icons/skills/trades/academics-book-study-runes.webp");
