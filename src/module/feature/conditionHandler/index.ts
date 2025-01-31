f417e29a110ee6092c9a53cd72f3407de8f22842
Jonas Karlsson <xdy@users.noreply.github.com>
2025-01-31 15:49:58 +0100

    fix: Reverts the previous "fix: Reducing frightened value should now reduce all non-locked frightened conditions on a character above the minimum frightened value by the appropriate amount."
    This reverts commit dfc96f2b046026c5e8cc199306d074b70f08fd1a.

--- src/module/feature/conditionHandler/index.ts
+++ src/module/feature/conditionHandler/index.ts
@@ -2,35 +2,24 @@ import { ActorPF2e, CombatantPF2e } from "foundry-pf2e";
 import { minionsInCurrentScene, shouldIHandleThis } from "../../utils.js";
 import { MODULENAME } from "../../xdy-pf2e-workbench.js";
 
-const FRIGHTENED = "frightened";
-const DWARVEN_DOUGHTINESS = "dwarven-doughtiness";
-
 export async function reduceFrightened(combatant: CombatantPF2e, userId: string) {
     if (!combatant || !combatant.actor || (userId !== game.user.id && !shouldIHandleThis(combatant.actor))) {
         return;
     }
 
-    const actorsToProcess: ActorPF2e[] = [combatant.actor, ...minionsInCurrentScene(combatant.actor)];
+    const actors: ActorPF2e[] = [combatant.actor, ...minionsInCurrentScene(combatant.actor)];
 
-    for (const actor of actorsToProcess) {
-        await processFrightenedActor(actor);
-    }
-}
+    for (const actor of actors) {
+        const minimumFrightened = <number>(actor?.getFlag(MODULENAME, "condition.frightened.min") ?? 0);
+        const frightened = actor.getCondition("frightened");
+        const currentFrightened = frightened?.value ?? 0;
 
-async function processFrightenedActor(actor: ActorPF2e): Promise<void> {
-    const minimumFrightened = Number(actor?.getFlag(MODULENAME, "condition.frightened.min") ?? 0);
-    const reduceBy = actor.itemTypes.feat.some((feat) => feat.slug === DWARVEN_DOUGHTINESS) ? 2 : 1;
+        if (frightened && currentFrightened > 0 && !frightened.isLocked) {
+            const reduceBy = actor.itemTypes.feat.some((feat) => feat.slug === "dwarven-doughtiness") ? 2 : 1;
 
-    let frightenedCondition = actor.getCondition(FRIGHTENED);
-    while (
-        frightenedCondition &&
-        frightenedCondition.value &&
-        frightenedCondition.value > minimumFrightened &&
-        !frightenedCondition.isLocked
-    ) {
-        for (let i = 0; i < reduceBy && frightenedCondition.value - i > minimumFrightened; i++) {
-            await actor.decreaseCondition(FRIGHTENED);
+            for (let i = 0; i < reduceBy && currentFrightened - i > minimumFrightened; i++) {
+                await actor.decreaseCondition("frightened");
+            }
         }
-        frightenedCondition = actor.getCondition(FRIGHTENED);
     }
 }
