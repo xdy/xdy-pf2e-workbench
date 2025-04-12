62a07b1925905707b2892a4f334a25d7e839982e
BuildTools <unconfigured@null.spigotmc.org>
2025-04-12 11:11:00 -0600

    Fix for Agile Maneuvers finally works

--- src/module/feature/macros/basicActionMacros.ts
+++ src/module/feature/macros/basicActionMacros.ts
@@ -110,7 +110,7 @@ type MacroAction = {
     best?: number;
     whoIsBest?: string;
     showMAP?: boolean;
-    showMAPAgile?: boolean;
+    isEscape?: boolean;
     showExploration?: boolean;
     showDowntime?: boolean;
 };
@@ -149,9 +149,9 @@ function prepareActions(selectedActor: ActorPF2e, bamActions: MacroAction[]): Ma
     actionsToUse.forEach((x) => {
         const traits = (x as any)?.action?.traits ?? [];
         x.showMAP = traits.includes("attack");
-		x.showMAPAgile = traits.includes("agile") && traits.includes("attack")
         x.showDowntime = traits.includes("downtime");
         x.showExploration = traits.includes("exploration");
+		x.isEscape = (x.name.includes("Escape"));
     });
 
     return actionsToUse;
